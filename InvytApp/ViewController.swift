93ea6f46fa19e6715a38f70160a47b444bc60b17
Julio Maldonado <Julio.A.Maldo@gmail.com>
2017-03-15 16:27:38 -0500

    Facebook login pods added

--- InvytApp/ViewController.swift
+++ InvytApp/ViewController.swift
@@ -12,9 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.isNavigationBarHidden = true
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
